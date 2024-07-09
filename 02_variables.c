efd27768e879e39bb68d566f7e51269ee544a456
Eklas-Mohin <mohin030299@gmail.com>
2024-07-10 05:30:20 +0600

    Introduction to variables in C

---
+++ 02_variables.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // Preprocessor directive
+
+int main() {
+    int a; // Variable declaration
+    a = 6; // Variable initialization
+    printf("Value stored in a is %d\n", a);
+    a = 1588; // Value can be changed
+    printf("Value stored in a is %d\n", a);
+    return 0;
+}
