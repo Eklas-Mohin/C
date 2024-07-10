3c9202c5414813601a46bf12e7ed9c7397d84a3a
Eklas-Mohin <mohin030299@gmail.com>
2024-07-10 21:12:59 +0600

    Arithmetic Operators in C

---
+++ 10_arithmetic_operators.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 3;
+    int c = b + a;
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    printf("a + b = %d\n", c);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %d\n", a / b);
+    printf("a %% b = %d\n", a % b); // Returns remainder
+    return 0;
+}
+
+/*
+    Arithmetic Operators
+    # Addition  +
+    # Subtraction -
+    # Multiplication *
+    # Division /
+    # Modulus %
+*/
