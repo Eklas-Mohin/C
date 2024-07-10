c3276aa7e70de2df80300c36dd90afd09eb18d53
Eklas-Mohin <mohin030299@gmail.com>
2024-07-10 08:41:53 +0600

    Taking input from keyboard in C

---
+++ 04_user_input.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf("Enter an integer: ");
+    scanf("%d", &a); // Taking input from keyboard
+    printf("Value of a = %d\n", a);
+    return 0;
+}
