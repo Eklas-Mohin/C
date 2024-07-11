7175fae465741deb8ff3ba0b34a52abdd4e84272
Eklas-Mohin <mohin030299@gmail.com>
2024-07-12 02:35:42 +0600

    ==  vs  =

--- 21_confusion.c
+++ 21_confusion.c
@@ -2,10 +2,28 @@
 
 int main() {
     int a = 10;
-    if (a = 11) {
+    if (a = 11) { // 11 is asigned to a and returns true
         printf("a is 11\n");
     } else {
-        printf("a is 10\n");
+        printf("a is not 11\n");
+    }
+    if (a == 11) {
+        printf("a is 11\n");
+    } else {
+        printf("a is not 11\n");
+    }
+    a = 10;
+    if (a == 11) {
+        printf("a is 11\n");
+    } else {
+        printf("a is not 11\n");
     }
     return 0;
 }
+
+/*  
+    Output:
+        a is 11
+        a is 11
+        a is not 11
+*/
