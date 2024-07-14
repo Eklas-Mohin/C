2aa2c42a4ce053d206962b2af7529df1ba757a63
Eklas-Mohin <mohin030299@gmail.com>
2024-07-14 07:06:13 +0600

    Function example

---
+++ 45_force_of_attraction.c
@@ -0,0 +1,24 @@
+/**
+*    author: mohin
+**/
+
+#include <stdio.h>
+
+// Function to calculate the force of attraction
+float attractionForce(float mass, float gravity) {
+    float force = mass * gravity;
+    return force;
+}
+
+int main() {
+    float mass;
+    float gravity_of_earth = 9.8;
+
+    printf("Enter mass of the object: ");
+    scanf("%f", &mass);
+
+    float force = attractionForce(mass, gravity_of_earth);
+    printf("Force of attraction exerted by Earth: %.2f\n", force);
+
+    return 0;
+}
