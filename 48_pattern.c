83aca0404aa6d72a7b9c24f9c8eb9dbbf2d1eb0f
Eklas-Mohin <mohin030299@gmail.com>
2024-07-15 22:09:18 +0600

    Pattern - 1

---
+++ 48_pattern.c
@@ -0,0 +1,24 @@
+/**
+*    author: mohin
+**/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter value for n : ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 1; j <= (2 * i + 1); ++j) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+/*
+    *
+    ***
+    *****
+*/
