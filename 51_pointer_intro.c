29a911aa9661c00b0461710ea038bdc80f71bbe6
Eklas-Mohin <mohin030299@gmail.com>
2024-07-16 22:45:16 +0600

    Introduction to Pointer in C

--- 51_pointer_intro.c
+++ 51_pointer_intro.c
@@ -9,8 +9,18 @@ int main() {
     printf("i = %d\n", i);
     printf("Address of i = %p\n", &i);
     int *k = &i; // Declaring and initializing a pointer variable
+    // k is pointing to i
     printf("Address of i = %p\n", k);
     printf("Value of i = %d\n", *k); // Dereferencing
+
+    int int_varibale = 565;
+    char char_varibale = 'H';
+    float float_variable = 1235.2588;
+
+    int* int_pointer = &int_varibale; // pointer to integer type variable
+    char* char_pointer = &char_varibale; // pointer to character type variable
+    float* float_pointer = &float_variable; // pointer to flaot type variable
+
     return 0;
 }
 
